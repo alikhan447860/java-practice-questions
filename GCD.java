00e9b754bc4249585585b9d102eae1ce7e7d8d98
Ali khan <khanali6861@gmail.com>
2024-09-26 20:01:13 +0530

    regular practice

--- GCD.java
+++ GCD.java
@@ -1,5 +1,11 @@
-public class GCD {
+public class GCD{
     public static void main(String[] args) {
-        int a=40,b=60;
+        int a=40,b=60,g=0;
+        for(int i=1;i<=a&&i<=b;i++){
+            if(a%i==0&&b%i==0){
+                g=i;
+            }
+        }
+        System.out.println("The gcd of a and b is "+g);
     }
 }
