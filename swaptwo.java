7e018fd33f9a1b2d0019fa4d2431ebd18df54ef9
Ali khan <khanali6861@gmail.com>
2024-09-27 10:44:08 +0530

    Regular Practice

--- swaptwo.java
+++ swaptwo.java
@@ -1,9 +1,9 @@
 public class swaptwo{
     public static void main(String[]args){
         int a=3,b=2;
-        a=a+b;
-        a=a-b;
-        b=a-b;
-        System.out.println(a+b);
+       a=a+b;
+       b=a-b;
+       a=a-b;
+        System.out.println(a+" "+b);
     }
 }
