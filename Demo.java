b043521fd81d816e3ee57a9d13866becf6d7da09
Ali khan <khanali6861@gmail.com>
2025-07-16 17:31:40 +0530

    second update

--- Demo.java
+++ Demo.java
@@ -1,15 +1,14 @@
+import java.util.*;
 public class Demo{
     public static void main(String[] args) {
-      int arr[]={1,2,3,4,1,1,5};
-      int start=0,maxlength=0,sum=0,k=5;
-      for(int end=0;end<arr.length;end++){
-             sum=sum+arr[end];
-             while(sum>k){
-                sum=sum-arr[start];
-                start++;
-             }
-             maxlength=Math.max(maxlength,end-start+1);
-      }
-      System.out.println("Length of the longest subarray with sum less than or equal to " + k + " is: " + maxlength);
+int arr[]={2,11,2,11,7,9};
+int target=9;
+     HashMap<Integer,Integer>map=new HashMap<>();
+     for(int val:arr){
+        map.put(val,map.getOrDefault(val,0 )+1);
+     }
+     for(int key:map.keySet()){
+        System.out.println("Element : "+key+" count : "+map.get(key));
+     }
     }
 }
