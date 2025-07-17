7de1f7a66e2f4632430fde4d4b9c2ec077d81574
Ali khan <khanali6861@gmail.com>
2025-07-17 16:11:56 +0530

    minor changes

--- Demo.java
+++ Demo.java
@@ -1,14 +1,23 @@
 import java.util.*;
 public class Demo{
     public static void main(String[] args) {
-int arr[]={2,11,2,11,7,9};
-int target=9;
-     HashMap<Integer,Integer>map=new HashMap<>();
-     for(int val:arr){
-        map.put(val,map.getOrDefault(val,0 )+1);
-     }
-     for(int key:map.keySet()){
-        System.out.println("Element : "+key+" count : "+map.get(key));
-     }
+        int arr[]={1,2,400,3,56,4};
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+int maxlen=0;
+for(int num:arr){
+    if(!set.contains(num-1)){
+        int current=num;
+        int count=1;
+        while(set.contains(current+1)){
+            count++;
+            current++;
+        }
+        maxlen=Math.max(maxlen,count);
     }
+}    
+System.out.println("The length of the longest consecutive sequence is : "+maxlen);
+}
 }
