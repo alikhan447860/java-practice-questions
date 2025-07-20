152b02af55de4794f45ebba12536a0be7e83787c
Ali khan <khanali6861@gmail.com>
2025-07-20 16:10:02 +0530

    first occ

--- Demo.java
+++ Demo.java
@@ -1,15 +1,15 @@
 public class Demo {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 2, 2, 3, 5, 9, 11, 33 };
-        int start = 0, end = arr.length - 1, tar = 5;
+        int start = 0, end = arr.length - 1, tar = 2;
         int ans = -1; // default if not found
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
             if (arr[mid] == tar) {
-                ans = mid; // store answer
-                end = mid - 1; // move left for first occurrence
+                ans = mid;        // store answer
+                end = mid - 1;    // move left for first occurrence
             } else if (arr[mid] < tar) {
                 start = mid + 1;
             } else {
