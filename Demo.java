9fbee2610c97ddf09de6ab3322bec3f02d1b3725
Ali khan <khanali6861@gmail.com>
2025-07-20 16:28:10 +0530

    print using recursion

--- Demo.java
+++ Demo.java
@@ -1,22 +1,13 @@
-public class Demo {
+public class Demo{
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 2, 2, 3, 5, 9, 11, 33 };
-        int start = 0, end = arr.length - 1, tar = 2;
-        int ans = -1; // default if not found
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (arr[mid] == tar) {
-                ans = mid;        // store answer
-                end = mid - 1;    // move left for first occurrence
-            } else if (arr[mid] < tar) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        print(5);
+    }
+    public static void print(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
         }
-
-        System.out.println("Element found at index: " + ans);
+        System.out.println(n);
+        print(n-1);
     }
 }
