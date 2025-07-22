c2bbe7878b5c3a22d96f5c9bdb565dafb20c6854
Ali khan <khanali6861@gmail.com>
2025-07-22 12:35:23 +0530

    BFS

---
+++ TreeBFS.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Node{
+    int data;
+    Node left,right;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+public class TreeBFS {
+    public static void bfs(Node root){
+       
+    }
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        System.out.println("BFS");
+        bfs(root);
+    }
+}
